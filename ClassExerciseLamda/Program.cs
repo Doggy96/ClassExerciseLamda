6ca8afc42c11d98ae1965d08bff94adacd68303a
Κωνσταντινος Σκυλακος <konsky@hotmail.gr>
2020-11-11 15:04:35 +0200

    Add project files.

---
+++ ClassExerciseLamda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExerciseLamda
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Home home = new Home();
+			home.Address = "ieremiou";
+			home.Name = "kostas";
+			PrintHome printHome;
+			printHome = name => Console.WriteLine(home.Name + home.Address); ;
+
+
+		}
+
+		public delegate void PrintHome(Home home);
+	}
+}
